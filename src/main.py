8df9a79ed746167e9c2e39c75327c9e74c7c65d8
Guinnes Singh <gswebspace@gmail.com>
2023-12-11 14:58:59 +0530

    WIP: Whitelisted origins for integration into dashboard

--- src/main.py
+++ src/main.py
@@ -1,20 +1,38 @@
 import logging
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.users import router as users_router
 from config import Settings
 from utils.db import init_db
 
 log = logging.getLogger("uvicorn")
 
-def create_application() -> FastAPI:
+whitelisted_origins = [
+    "http://amb-dev.embetter.in",
+    "https://amb-dev.embetter.in",
+    "http://ambition.fund",
+    "https://ambition.fund",
+    "http://dev.ambition.fund",
+    "https://dev.ambition.fund",
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+def create_application(whitelisted_origins) -> FastAPI:
     settings = Settings()
     application = FastAPI(title=settings.name, version=settings.version)
     application.include_router(users_router, prefix="/users")
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=whitelisted_origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return application
 
-app = create_application()
+app = create_application(whitelisted_origins)
 
 @app.on_event("startup")
 async def startup_event():
